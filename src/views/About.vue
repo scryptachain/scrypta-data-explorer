6a5b5260b454bdcd99b10733afb53332bd8a484b
turinglabsorg <turinglabs@tuta.io>
2019-09-11 15:11:28 +0200

    init commit

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about" style="padding-top:25px">
+    <img alt="Vue logo" src="../assets/logo.png" width="100"><br><br>
+    <h3>Scrypta Proof is a Data Explorer<br>for the Scrypta Blockchain</h3><br>
+    v0.0.1
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <Explorer/>
+  </div>
+</template>
+
+<script>
+import Explorer from '@/components/Explorer.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Explorer
+  }
+}
+</script>
