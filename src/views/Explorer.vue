72e743b690cdf33a276af670829ac34a1f3668e4
turinglabsorg <turinglabs@criptext.com>
2020-02-04 09:28:37 +0100

    added unconfirmed transactions

--- src/views/Explorer.vue
+++ src/views/Explorer.vue
@@ -5,8 +5,31 @@
         <div v-if="last.length === 0" style="padding:45vh 0">
           Loading data, please wait...
         </div>
+        <div v-if="unconfirmedTxs">
+          <h3 style="margin-top:30px">Unconfirmed transactions</h3>
+          <div v-for="data in last" v-bind:key="data.uuid">
+            <div class="card" style="width: 100%; margin-top:20px" v-if="data.block === undefined">
+              <div class="card-body">
+                <h5 class="card-title" v-if="data.title !== undefined && data.title !== 'undefined' && data.title !== ''">{{ data.title }}</h5>
+                <h5 class="card-title" v-if="data.refID !== undefined && data.refID !== 'undefined' && data.refID !== ''">{{ data.refID }}</h5>
+                <div v-if="data.mime">
+                  <img :src="idanode + '/ipfs/' + data.data" width="100%" v-if="data.mime.type === 'image'">
+                  IPFS hash: <a :href="idanode + '/ipfs/' + data.data" target="_blank">{{data.data}}</a><br>
+                  <span v-if="data.mime.mime">File Type: {{ data.mime.mime.toUpperCase() }}<br></span>
+                </div>
+                <p v-if="data.is_file === false" class="card-text">{{ data.data }}</p>
+                UUID is <a :href="'/#/uuid/' + data.uuid">{{ data.uuid }}</a><br>
+                Written by <a :href="'/#/address/' + data.address">{{ data.address }}</a> 
+                <hr>
+                <a :href="'/#/uuid/' + data.uuid" style="margin: 10px" class="btn btn-primary">Show details</a>
+                <a v-if="data.is_file === true" :href="idanode + '/ipfs/' + data.data" target="_blank" style="margin: 10px" class="btn btn-primary">Download file</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h3 style="margin-top:30px">Confirmed transactions</h3>
         <div v-for="data in last" v-bind:key="data.uuid">
-          <div class="card" style="width: 100%; margin-top:20px">
+          <div class="card" style="width: 100%; margin-top:20px" v-if="data.block > 0">
             <div class="card-body">
               <h5 class="card-title" v-if="data.title !== undefined && data.title !== 'undefined' && data.title !== ''">{{ data.title }}</h5>
               <h5 class="card-title" v-if="data.refID !== undefined && data.refID !== 'undefined' && data.refID !== ''">{{ data.refID }}</h5>
@@ -40,7 +63,8 @@ export default {
       idanode: '',
       axios: axios,
       last: [],
-      bans: bans
+      bans: bans,
+      unconfirmedTxs: false
     }
   },
   async mounted() {
@@ -66,6 +90,9 @@ export default {
             app.last.push(readreturn.data.data[i])
           }else{
             app.last.push(readreturn.data.data[i])
+          }
+          if(readreturn.data.data[i].block === undefined){
+            app.unconfirmedTxs = true
           }
         }
       }
